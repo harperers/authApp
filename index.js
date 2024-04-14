bfd745a4d8a4b795e6d477a516b7f9c85cee1d6d
harperers <max@pan-ami.com>
2024-04-14 16:01:59 -0700

    day 2 hw commit

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const bodyParser = require('body-parser');
 const expressSession = require('express-session')({
     secret: 'secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 60000
+    }
 });
 
 app.use(bodyParser.json());
@@ -105,6 +109,6 @@ app.get('/logout',
 
 //Register Some Users
 
-UserDetails.register({username:'paul', active: false}, 'paul');
-UserDetails.register({username:'joy', active: false}, 'joy');
-UserDetails.register({username:'ray', active: false}, 'ray');
+//UserDetails.register({username:'paul', active: false}, 'paul');
+//UserDetails.register({username:'joy', active: false}, 'joy');
+//UserDetails.register({username:'ray', active: false}, 'ray');
